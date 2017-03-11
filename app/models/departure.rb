ad1c48167baae19f2d346eb4899309908f5190e2
Pablo <pablo.ledesma.p@gmail.com>
2017-03-11 16:46:06 -0500

    departure model and test

--- app/models/departure.rb
+++ app/models/departure.rb
@@ -1,3 +1,10 @@
 class Departure < ActiveRecord::Base
   belongs_to :status
+  # belongs_to :trip
+  # belongs_to :track
+  #
+  # validates :status_id, presence: true
+  # validates :trip_id, presence: true
+  validates :lateness, presence: true
+  validates :created_at, presence: true
 end
--- spec/models/status_spec.rb
+++ spec/models/status_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
 RSpec.describe Status, type: :model do
-  it { should validate_presence_of :name}
   it { should have_many(:departures) }
+  it { should validate_presence_of :name }
 end
