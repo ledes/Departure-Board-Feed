7b69d730b6b8e55f7df134a6a7f120dd252b131a
Pablo <pablo.ledesma.p@gmail.com>
2017-03-12 12:09:52 -0400

    initial components

--- app/assets/javascripts/components/Test.jsx
+++
@@ -1,8 +0,0 @@
-var Test = React.createClass({
-  render: function() {
-    debugger;
-    return (
-      <div>{}</div>
-    )
-  }
-})
--- app/controllers/departures_controller.rb
+++ app/controllers/departures_controller.rb
@@ -2,7 +2,7 @@ class DeparturesController < ApplicationController
 
   def index
     @departures = Departure.all
-     render component: 'Test', props: { departures: @departures }, tag: 'span', class: 'todo'
+    render component: 'DeparturesBoard', props: { departures: @departures }, tag: 'span', class: 'departures-board'
   end
 
 end
