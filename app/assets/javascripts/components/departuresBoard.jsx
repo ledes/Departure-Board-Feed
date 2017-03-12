07794a8abd2766978d1ce6559c8733b6212722a6
Pablo <pablo.ledesma.p@gmail.com>
2017-03-12 15:52:03 -0400

    dates and time

--- app/assets/javascripts/components/departuresBoard.jsx
+++ app/assets/javascripts/components/departuresBoard.jsx
@@ -5,13 +5,52 @@ var DeparturesBoard = React.createClass({
   },
 
   tableHeaders: function() {
-    return (['Origin', 'Destination', 'Trip', 'Time', 'Track', 'Status']);
+    return (['Time', 'Origin', 'Trip', 'Destination', 'Track', 'Status']);
   },
+
+  //=== Dates and Time ===
+
+  renderDayOfTheWeek: function(){
+    var dateToday = new Date();
+    var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
+    return days[dateToday.getDay()];
+  },
+
+  renderFormadtedDated: function() {
+    var dateToday = new Date();
+    var mm = dateToday.getMonth() + 1;
+    var dd = dateToday.getDate();
+    var yy = dateToday.getFullYear();
+
+    return mm + '-' +  dd + '-' + yy;
+  },
+
+  renderCurrentTime: function() {
+    var dateToday = new Date();
+
+    return dateToday.toLocaleString(
+      'en-US',
+      { hour: 'numeric', minute: 'numeric', hour12: true }
+    );
+  },
+
+  //=== Render ===
 
   render: function() {
     return (
       <div>
-        <div>Departures Board</div>
+        <div className={'top-info'}>
+          <div className='current-day-box'>
+            <div>{this.renderDayOfTheWeek()}</div>
+            <div>{this.renderFormadtedDated()}</div>
+          </div>
+          <div>Departures Board</div>
+          <div className='current-time-box'>
+            <div>Current Time</div>
+            <div>{this.renderCurrentTime()}</div>
+          </div>
+        </div>
         <div><Table headers={this.tableHeaders()} rows={this.props.tableData}/></div>
       </div>
     );
