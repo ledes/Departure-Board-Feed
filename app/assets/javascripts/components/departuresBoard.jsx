c1cc2d39e8d0f6277ac9302b0cab25a3c8f89a15
Pablo <pablo.ledesma.p@gmail.com>
2017-03-12 20:25:57 -0400

    fix turbolinks and filter by station

--- app/assets/javascripts/components/departuresBoard.jsx
+++ app/assets/javascripts/components/departuresBoard.jsx
@@ -4,12 +4,17 @@ var DeparturesBoard = React.createClass({
     tableData: React.PropTypes.arrayOf(React.PropTypes.object).isRequired,
   },
 
+  getInitialState: function() {
+    return {
+      origin: 'North Station'
+    };
+  },
+
   tableHeaders: function() {
     return (['Time', 'Origin', 'Trip', 'Destination', 'Track', 'Status']);
   },
 
   //=== Dates and Time ===
-
   renderDayOfTheWeek: function(){
     var dateToday = new Date();
     var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
@@ -35,8 +40,15 @@ var DeparturesBoard = React.createClass({
     );
   },
 
-  //=== Render ===
+  //=== Filters ===
+  filterOrigin: function() {
+    var state = this.state;
+    return _.filter(this.props.tableData, function(trip){
+      return trip.origin === state.origin;
+    })
+  },
 
+  //=== Render ===
   render: function() {
     return (
       <div id='departures-board-page'>
@@ -46,13 +58,13 @@ var DeparturesBoard = React.createClass({
               <div>{this.renderDayOfTheWeek()}</div>
               <div>{this.renderFormadtedDated()}</div>
             </div>
-            <div className='central-top-container'>Departures Board</div>
+            <div className='central-top-container'>{this.state.origin + ' Departures Board'}</div>
             <div className='top-container'>
               <div>Current Time</div>
               <div>{this.renderCurrentTime()}</div>
             </div>
           </div>
-          <Table headers={this.tableHeaders()} rows={this.props.tableData}/>
+          <Table headers={this.tableHeaders()} rows={this.filterOrigin()}/>
         </div>
       </div>
     );
